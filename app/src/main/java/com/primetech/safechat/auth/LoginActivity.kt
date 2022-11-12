1c999040cf6c5b72f0600d26c6a4d6c6f5a0a39a
Krisna Sirait <krisnapande00@outlook.com>
2022-11-12 22:32:54 +0700

    added function for text register

--- app/src/main/java/com/primetech/safechat/auth/LoginActivity.kt
+++ app/src/main/java/com/primetech/safechat/auth/LoginActivity.kt
@@ -1,8 +1,10 @@
 package com.primetech.safechat.auth
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.primetech.safechat.MainActivity
 import com.primetech.safechat.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -16,5 +18,15 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setOnClickListener()
+    }
+
+    private fun setOnClickListener() {
+        binding.tvRegister.setOnClickListener {
+            Intent(this,MainActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
