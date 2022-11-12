2b9282a95e91d3fa26ad0503ad5ca7ca9907c40d
Krisna Sirait <krisnapande00@outlook.com>
2022-11-12 22:14:52 +0700

    created activity register

---
+++ app/src/main/java/com/primetech/safechat/auth/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.primetech.safechat.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.primetech.safechat.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
