d554b93f614005478b0086efbb1df6e260d36301
Krisna Sirait <krisnapande00@outlook.com>
2022-11-12 22:34:21 +0700

    added function for login activity in RegisterActivity.kt

--- app/src/main/java/com/primetech/safechat/auth/RegisterActivity.kt
+++ app/src/main/java/com/primetech/safechat/auth/RegisterActivity.kt
@@ -1,12 +1,29 @@
 package com.primetech.safechat.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.primetech.safechat.R
+import com.primetech.safechat.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        setOnClickListener()
+    }
+
+    private fun setOnClickListener() {
+        binding.tvLogin.setOnClickListener {
+            Intent(this, LoginActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
