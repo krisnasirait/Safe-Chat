30d3eb5653d58fc23f02f121caf41ad979a6a3fb
Krisna Sirait <krisnapande00@outlook.com>
2022-11-12 21:57:07 +0700

    added view binding LoginActivity.kt

--- app/src/main/java/com/primetech/safechat/auth/LoginActivity.kt
+++ app/src/main/java/com/primetech/safechat/auth/LoginActivity.kt
@@ -3,10 +3,15 @@ package com.primetech.safechat.auth
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.primetech.safechat.R
+import com.primetech.safechat.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
