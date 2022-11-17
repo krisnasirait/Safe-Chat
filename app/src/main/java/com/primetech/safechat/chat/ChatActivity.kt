bf0993b8c43b7e4bf9220a6591e9bc5864f525a1
Krisna Sirait <krisnapande00@outlook.com>
2022-11-17 15:54:30 +0700

    created chat and inbox activity file

---
+++ app/src/main/java/com/primetech/safechat/chat/ChatActivity.kt
@@ -0,0 +1,12 @@
+package com.primetech.safechat.chat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.primetech.safechat.R
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
---
+++ app/src/main/java/com/primetech/safechat/chat/InboxActivity.kt
@@ -0,0 +1,12 @@
+package com.primetech.safechat.chat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.primetech.safechat.R
+
+class InboxActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inbox)
+    }
+}
